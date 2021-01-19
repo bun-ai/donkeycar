bdcdb16b272dd89970500118e1625b318aa37051
bun_ai <bunalil@mail.ru>
2021-01-19 20:31:26 +0300

    Used Black on scripts in donkeycar/donkeycar folder.

--- donkeycar/config.py
+++ donkeycar/config.py
@@ -9,24 +9,23 @@ import types
 
 
 class Config:
-    
     def from_pyfile(self, filename):
-        d = types.ModuleType('config')
+        d = types.ModuleType("config")
         d.__file__ = filename
         try:
-            with open(filename, mode='rb') as config_file:
-                exec(compile(config_file.read(), filename, 'exec'), d.__dict__)
+            with open(filename, mode="rb") as config_file:
+                exec(compile(config_file.read(), filename, "exec"), d.__dict__)
         except IOError as e:
-            e.strerror = 'Unable to load configuration file (%s)' % e.strerror
+            e.strerror = "Unable to load configuration file (%s)" % e.strerror
             raise
         self.from_object(d)
         return True
-    
+
     def from_object(self, obj):
         for key in dir(obj):
             if key.isupper():
                 setattr(self, key, getattr(obj, key))
-                
+
     def __str__(self):
         result = []
         for key in dir(self):
@@ -41,17 +40,18 @@ class Config:
 
 
 def load_config(config_path=None, myconfig="myconfig.py"):
-    
+
     if config_path is None:
         import __main__ as main
+
         main_path = os.path.dirname(os.path.realpath(main.__file__))
-        config_path = os.path.join(main_path, 'config.py')
+        config_path = os.path.join(main_path, "config.py")
         if not os.path.exists(config_path):
-            local_config = os.path.join(os.path.curdir, 'config.py')
+            local_config = os.path.join(os.path.curdir, "config.py")
             if os.path.exists(local_config):
                 config_path = local_config
-    
-    print('loading config file: {}'.format(config_path))
+
+    print("loading config file: {}".format(config_path))
     cfg = Config()
     cfg.from_pyfile(config_path)
 
@@ -66,7 +66,7 @@ def load_config(config_path=None, myconfig="myconfig.py"):
         print("personal config: file not found ", personal_cfg_path)
 
     # derived settings
-    if hasattr(cfg, 'IMAGE_H') and hasattr(cfg, 'IMAGE_W'): 
+    if hasattr(cfg, "IMAGE_H") and hasattr(cfg, "IMAGE_W"):
         cfg.TARGET_H = cfg.IMAGE_H
         cfg.TARGET_W = cfg.IMAGE_W
         cfg.TARGET_D = cfg.IMAGE_DEPTH
--- donkeycar/utils.py
+++ donkeycar/utils.py
@@ -1,9 +1,9 @@
-'''
+"""
 utils.py
 
 Functions that don't fit anywhere else.
 
-'''
+"""
 from io import BytesIO
 import os
 import glob
@@ -21,28 +21,27 @@ from typing import List, Any, Tuple
 from PIL import Image
 import numpy as np
 
-
-'''
+"""
 IMAGES
-'''
+"""
 ONE_BYTE_SCALE = 1.0 / 255.0
 
 
 def scale(im, size=128):
-    '''
+    """
     accepts: PIL image, size of square sides
     returns: PIL image scaled so sides lenght = size
-    '''
-    size = (size,size)
+    """
+    size = (size, size)
     im.thumbnail(size, Image.ANTIALIAS)
     return im
 
 
-def img_to_binary(img, format='jpeg'):
-    '''
+def img_to_binary(img, format="jpeg"):
+    """
     accepts: PIL image
     returns: binary stream (used to save to database)
-    '''
+    """
     f = BytesIO()
     try:
         img.save(f, format=format)
@@ -52,37 +51,37 @@ def img_to_binary(img, format='jpeg'):
 
 
 def arr_to_binary(arr):
-    '''
+    """
     accepts: numpy array with shape (Hight, Width, Channels)
     returns: binary stream (used to save to database)
-    '''
+    """
     img = arr_to_img(arr)
     return img_to_binary(img)
 
 
 def arr_to_img(arr):
-    '''
+    """
     accepts: numpy array with shape (Height, Width, Channels)
     returns: binary stream (used to save to database)
-    '''
+    """
     arr = np.uint8(arr)
     img = Image.fromarray(arr)
     return img
 
 
 def img_to_arr(img):
-    '''
+    """
     accepts: PIL image
     returns: a numpy uint8 image
-    '''
+    """
     return np.array(img)
 
 
 def binary_to_img(binary):
-    '''
+    """
     accepts: binary file object from BytesIO
     returns: PIL image
-    '''
+    """
     if binary is None or len(binary) == 0:
         return None
 
@@ -155,13 +154,13 @@ def load_pil_image(filename, cfg):
             img = img.resize((cfg.IMAGE_W, cfg.IMAGE_H))
 
         if cfg.IMAGE_DEPTH == 1:
-            img = img.convert('L')
-        
+            img = img.convert("L")
+
         return img
 
     except Exception as e:
         print(e)
-        print('failed to load image:', filename)
+        print("failed to load image:", filename)
         return None
 
 
@@ -180,22 +179,23 @@ def load_image(filename, cfg):
 
     # If the PIL image is greyscale, the np array will have shape (H, W)
     # Need to add a depth channel by expanding to (H, W, 1)
-    if img.mode == 'L':
+    if img.mode == "L":
         h, w = img_arr.shape[:2]
         img_arr = img_arr.reshape(h, w, 1)
 
     return img_arr
 
-'''
+
+"""
 FILES
-'''
+"""
 
 
-def most_recent_file(dir_path, ext=''):
-    '''
+def most_recent_file(dir_path, ext=""):
+    """
     return the most recent file given a directory path and extension
-    '''
-    query = dir_path + '/*' + ext
+    """
+    query = dir_path + "/*" + ext
     newest = min(glob.iglob(query), key=os.path.getctime)
     return newest
 
@@ -211,9 +211,9 @@ def zip_dir(dir_path, zip_path):
     """
     Create and save a zipfile of a one level directory
     """
-    file_paths = glob.glob(dir_path + "/*") #create path to search for files.
+    file_paths = glob.glob(dir_path + "/*")  # create path to search for files.
 
-    zf = zipfile.ZipFile(zip_path, 'w')
+    zf = zipfile.ZipFile(zip_path, "w")
     dir_name = os.path.basename(dir_path)
     for p in file_paths:
         file_name = os.path.basename(p)
@@ -222,11 +222,10 @@ def zip_dir(dir_path, zip_path):
     return zip_path
 
 
-
-'''
+"""
 BINNING
 functions to help converte between floating point numbers and categories.
-'''
+"""
 
 
 def clamp(n, min, max):
@@ -238,11 +237,11 @@ def clamp(n, min, max):
 
 
 def linear_bin(a, N=15, offset=1, R=2.0):
-    '''
+    """
     create a bin of length N
     map val A to range R
     offset one hot bin by offset, commonly R/2
-    '''
+    """
     a = a + offset
     b = round(a / (R / (N - offset)))
     arr = np.zeros(N)
@@ -252,46 +251,47 @@ def linear_bin(a, N=15, offset=1, R=2.0):
 
 
 def linear_unbin(arr, N=15, offset=-1, R=2.0):
-    '''
+    """
     preform inverse linear_bin, taking
     one hot encoded arr, and get max value
     rescale given R range and offset
-    '''
+    """
     b = np.argmax(arr)
     a = b * (R / (N + offset)) + offset
     return a
 
 
 def map_range(x, X_min, X_max, Y_min, Y_max):
-    '''
+    """
     Linear mapping between two ranges of values
-    '''
+    """
     X_range = X_max - X_min
     Y_range = Y_max - Y_min
-    XY_ratio = X_range/Y_range
+    XY_ratio = X_range / Y_range
 
-    y = ((x-X_min) / XY_ratio + Y_min) // 1
+    y = ((x - X_min) / XY_ratio + Y_min) // 1
 
     return int(y)
 
 
 def map_range_float(x, X_min, X_max, Y_min, Y_max):
-    '''
+    """
     Same as map_range but supports floats return, rounded to 2 decimal places
-    '''
+    """
     X_range = X_max - X_min
     Y_range = Y_max - Y_min
-    XY_ratio = X_range/Y_range
+    XY_ratio = X_range / Y_range
 
-    y = ((x-X_min) / XY_ratio + Y_min)
+    y = (x - X_min) / XY_ratio + Y_min
 
     # print("y= {}".format(y))
 
-    return round(y,2)
+    return round(y, 2)
 
-'''
+
+"""
 ANGLES
-'''
+"""
 
 
 def norm_deg(theta):
@@ -308,31 +308,33 @@ DEG_TO_RAD = math.pi / 180.0
 def deg2rad(theta):
     return theta * DEG_TO_RAD
 
-'''
+
+"""
 VECTORS
-'''
+"""
 
 
 def dist(x1, y1, x2, y2):
     return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
 
-'''
+"""
 NETWORKING
-'''
+"""
 
 
 def my_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('192.0.0.8', 1027))
+    s.connect(("192.0.0.8", 1027))
     return s.getsockname()[0]
 
-'''
-THROTTLE
-'''
 
-STEERING_MIN = -1.
-STEERING_MAX = 1.
+"""
+THROTTLE
+"""
+
+STEERING_MIN = -1.0
+STEERING_MAX = 1.0
 # Scale throttle ~ 0.5 - 1.0 depending on the steering angle
 EXP_SCALING_FACTOR = 0.5
 DAMPENING = 0.05
@@ -340,29 +342,30 @@ DAMPENING = 0.05
 
 def _steering(input_value):
     input_value = clamp(input_value, STEERING_MIN, STEERING_MAX)
-    return ((input_value - STEERING_MIN) / (STEERING_MAX - STEERING_MIN))
+    return (input_value - STEERING_MIN) / (STEERING_MAX - STEERING_MIN)
 
 
 def throttle(input_value):
     magnitude = _steering(input_value)
     decay = math.exp(magnitude * EXP_SCALING_FACTOR)
     dampening = DAMPENING * magnitude
-    return ((1 / decay) - dampening)
+    return (1 / decay) - dampening
 
-'''
+
+"""
 OTHER
-'''
+"""
 
 
 def map_frange(x, X_min, X_max, Y_min, Y_max):
-    '''
+    """
     Linear mapping between two ranges of values
-    '''
+    """
     X_range = X_max - X_min
     Y_range = Y_max - Y_min
-    XY_ratio = X_range/Y_range
+    XY_ratio = X_range / Y_range
 
-    y = ((x-X_min) / XY_ratio + Y_min)
+    y = (x - X_min) / XY_ratio + Y_min
 
     return y
 
@@ -375,16 +378,18 @@ def merge_two_dicts(x, y):
 
 
 def param_gen(params):
-    '''
+    """
     Accepts a dictionary of parameter options and returns
     a list of dictionary with the permutations of the parameters.
-    '''
+    """
     for p in itertools.product(*params.values()):
-        yield dict(zip(params.keys(), p ))
+        yield dict(zip(params.keys(), p))
 
 
 def run_shell_command(cmd, cwd=None, timeout=15):
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
+    proc = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd
+    )
     out = []
     err = []
 
@@ -409,27 +414,33 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def get_model_by_type(model_type: str, cfg: 'Config') -> 'KerasPilot':
-    '''
+def get_model_by_type(model_type: str, cfg: "Config") -> "KerasPilot":
+    """
     given the string model_type and the configuration settings in cfg
     create a Keras model and return it.
-    '''
-    from donkeycar.parts.keras import KerasPilot, KerasCategorical, \
-        KerasLinear, KerasInferred
+    """
+    from donkeycar.parts.keras import (
+        KerasPilot,
+        KerasCategorical,
+        KerasLinear,
+        KerasInferred,
+    )
     from donkeycar.parts.tflite import TFLitePilot
 
     if model_type is None:
         model_type = cfg.DEFAULT_MODEL_TYPE
-    print("\"get_model_by_type\" model Type is: {}".format(model_type))
+    print('"get_model_by_type" model Type is: {}'.format(model_type))
 
     input_shape = (cfg.IMAGE_H, cfg.IMAGE_W, cfg.IMAGE_DEPTH)
     kl: KerasPilot
     if model_type == "linear":
         kl = KerasLinear(input_shape=input_shape)
     elif model_type == "categorical":
-        kl = KerasCategorical(input_shape=input_shape,
-                              throttle_range=cfg.MODEL_CATEGORICAL_MAX_THROTTLE_RANGE)
-    elif model_type == 'inferred':
+        kl = KerasCategorical(
+            input_shape=input_shape,
+            throttle_range=cfg.MODEL_CATEGORICAL_MAX_THROTTLE_RANGE,
+        )
+    elif model_type == "inferred":
         kl = KerasInferred(input_shape=input_shape)
     elif model_type == "tflite_linear":
         kl = TFLitePilot()
@@ -438,12 +449,14 @@ def get_model_by_type(model_type: str, cfg: 'Config') -> 'KerasPilot':
         # which causes a lot of unexpected things to happen when using TF-GPU
         # for training.
         from donkeycar.parts.tensorrt import TensorRTLinear
+
         kl = TensorRTLinear(cfg=cfg)
     else:
-        raise Exception("Unknown model type {:}, supported types are "
-                        "linear, categorical, inferred, tflite_linear, "
-                        "tensorrt_linear"
-                        .format(model_type))
+        raise Exception(
+            "Unknown model type {:}, supported types are "
+            "linear, categorical, inferred, tflite_linear, "
+            "tensorrt_linear".format(model_type)
+        )
 
     return kl
 
@@ -455,7 +468,7 @@ def get_test_img(model):
     :param model:                   input keras model
     :return np.ndarry(np.uint8):    numpy random img array
     """
-    assert(len(model.inputs) > 0)
+    assert len(model.inputs) > 0
     try:
         count, h, w, ch = model.inputs[0].get_shape()
         seq_len = 0
@@ -467,16 +480,16 @@ def get_test_img(model):
     return img.astype(np.uint8)
 
 
-def train_test_split(data_list: List[Any],
-                     shuffle: bool = True,
-                     test_size: float = 0.2) -> Tuple[List[Any], List[Any]]:
-    '''
+def train_test_split(
+    data_list: List[Any], shuffle: bool = True, test_size: float = 0.2
+) -> Tuple[List[Any], List[Any]]:
+    """
     take a list, split it into two sets while selecting a
     random element in order to shuffle the results.
     use the test_size to choose the split percent.
     shuffle is always True, left there to be backwards compatible
-    '''
-    target_train_size = int(len(data_list) * (1. - test_size))
+    """
+    target_train_size = int(len(data_list) * (1.0 - test_size))
 
     if shuffle:
         train_data = []
@@ -514,6 +527,6 @@ class FPSTimer(object):
         self.iter += 1
         if self.iter == 100:
             e = time.time()
-            print('fps', 100.0 / (e - self.t))
+            print("fps", 100.0 / (e - self.t))
             self.t = time.time()
             self.iter = 0
